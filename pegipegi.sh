6f040af9f56b65b7a44add24c7d421e6ab82bb27
mtezy <miftah3rdu22@gmail.com>
2020-08-19 23:13:01 +0700

    Update pegipegi.sh

--- pegipegi.sh
+++ pegipegi.sh
@@ -43,7 +43,7 @@ ngecek() {
     username=$(echo ${1} | cut -d@ -f1)
     extract=$(echo ${2} | awk '{ print tolower($0); }')
     postdata="button[country_code]=US&button[type]=DONATE&button[sub_type]=PRODUCTS&button[language]=en&button[button_variables][0][name]=item_name&button[button_variables][0][value]=&button[button_variables][1][name]=currency_code&button[button_variables][1][value]=USD&button[button_variables][2][name]=business&button[button_variables][2][value]=${1}&button[button_variables][3][name]=item_number&button[button_variables][3][value]=&button[button_variables][4][name]=no_shipping&button[button_variables][4][value]=1&button[button_variables][5][name]=no_note&button[button_variables][5][value]=0&button[button_image_url]=https%3A%2F%2Fwww.paypalobjects.com%2Fen_US%2Fi%2Fbtn%2Fbtn_donateCC_LG.gif&button[button_image]=CC"
-	authPage=$(curl -s --socks5 127.0.0.1:9050 "https://serviceauth.pegipegi.com/login" -i \
+	authPage=$(curl -s "https://serviceauth.pegipegi.com/login" -i \
 	-H "Accept-Language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7" \
 	-H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36")
 #  auth=$(echo "$setCok" | grep __VIEWSTATE | sort -u | grep -Po '(?<=value=")[^"]*' | sed -e '/^CA0B0334/,/^CA0B0334/d')
@@ -52,7 +52,7 @@ ngecek() {
   token=$(echo $authPage | grep csrf-sso | grep -Po '(?<=value=")[^"]*' | head -n1)
   auth=$(echo $authPage | grep csrf-sso | grep -Po '(?<=value=")[^"]*')
   auths=( $auth )
-  gasLogin=$(curl -s -i --socks5 127.0.0.1:9050 "https://serviceauth.pegipegi.com/login" -X POST \
+  gasLogin=$(curl -s -i "https://serviceauth.pegipegi.com/login" -X POST \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -H "Referer: https://serviceauth.pegipegi.com/login?resource=sp_main_homepage&redirect=https://www.pegipegi.com" \
   -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36" \
@@ -65,7 +65,7 @@ ngecek() {
   if [[ $setcookie == *"sso"* ]]; then
   sso=$(echo "$setcookie" | grep -Po '(?<=sso=)[^;]*')
   phpses=$(echo "$setcookie" | grep -Po '(?<=phpsess=)[^;]*')
-  getReward=$(curl -s --socks5 127.0.0.1:9050 "https://serviceauth.pegipegi.com/account/profile/reward" -X GET \
+  getReward=$(curl -s "https://serviceauth.pegipegi.com/account/profile/reward" -X GET \
   -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36" \
   -H "Cookie: sso=${sso}; phpsess=${phpses}; authservice=${setCok}")
   totalpoint=$(echo "$getReward" | sed -e 's/<[^>]*>//g' | grep -Po '(?<=Total Point Anda : )[^"]*' | tr -d '()')
